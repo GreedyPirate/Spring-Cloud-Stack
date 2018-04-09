30defc3f6f47ffa97ce56f9f31efd38cdfe127f6
yangjie <yangjie@songguo7.com>
2018-04-09 20:57:14 +0800

    module

---
+++ consumer-hystrix/src/test/java/com/ttyc/consumer/hystrix/ConsumerHystrixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ttyc.consumer.hystrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumerHystrixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
