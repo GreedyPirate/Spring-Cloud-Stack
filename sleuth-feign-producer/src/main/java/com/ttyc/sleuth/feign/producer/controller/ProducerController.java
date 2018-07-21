ff926f5eeb081ae46703e16d38905d423b9a9a2f
yangjie <yangjie@songguo7.com>
2018-07-21 17:14:22 +0800

    zipkin elasticsearch

--- sleuth-feign-producer/src/main/java/com/ttyc/sleuth/feign/producer/controller/ProducerController.java
+++ sleuth-feign-producer/src/main/java/com/ttyc/sleuth/feign/producer/controller/ProducerController.java
@@ -1,8 +1,8 @@
 package com.ttyc.sleuth.feign.producer.controller;
 
+import com.ttyc.sleuth.feign.producer.model.User;
 import com.ttyc.sleuth.feign.producer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ public class ProducerController {
     UserRepository userRepository;
 
     @GetMapping("hi")
-    public String hi(){
-        return this.userRepository.findById(1l).toString();
+    public User hi(){
+        return this.userRepository.findById(1L).get();
     }
 }
--- zuul-swagger-butler/src/main/java/com/example/zuul/swagger/butler/ZuulSwaggerButlerApplication.java
+++ zuul-swagger-butler/src/main/java/com/example/zuul/swagger/butler/ZuulSwaggerButlerApplication.java
@@ -1,9 +1,11 @@
 package com.example.zuul.swagger.butler;
 
+import com.didispace.swagger.butler.EnableSwaggerButler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwaggerButler
 public class ZuulSwaggerButlerApplication {
 
 	public static void main(String[] args) {
