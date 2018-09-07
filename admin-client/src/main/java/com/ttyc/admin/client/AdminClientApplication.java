5daf753a55807fa9a0abaaf5071dd71d5d4bd02e
yangjie <yangjie@songguo7.com>
2018-09-07 14:31:48 +0800

    admin server 集成eureka

---
+++ admin-client/src/main/java/com/ttyc/admin/client/AdminClientApplication.java
@@ -0,0 +1,12 @@
+package com.ttyc.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminClientApplication.class, args);
+	}
+}
