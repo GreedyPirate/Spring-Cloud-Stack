3191ca9aeb71cc817215135dcc86521752b04951
yangjie <yangjie@songguo7.com>
2018-09-09 23:40:06 +0800

    静态配置admin 客户端

---
+++ admin-static-client/src/main/java/com/ttyc/admin/client/AdminStaticClientApplication.java
@@ -0,0 +1,12 @@
+package com.ttyc.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminStaticClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminStaticClientApplication.class, args);
+	}
+}
