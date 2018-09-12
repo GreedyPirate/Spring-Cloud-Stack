9724a7ba029fce66c87705e982b254ea6bfa5c07
GreedyPirate <18335166203@163.com>
2018-09-12 15:54:44 +0800

    通知api示例

--- admin-server/src/main/java/com/ttyc/admin/server/config/MyNotifier.java
+++ admin-server/src/main/java/com/ttyc/admin/server/config/MyNotifier.java
@@ -4,11 +4,14 @@ import de.codecentric.boot.admin.server.domain.entities.Instance;
 import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
 import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
 import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.domain.values.StatusInfo;
 import de.codecentric.boot.admin.server.notify.AbstractStatusChangeNotifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Component
 public class MyNotifier extends AbstractStatusChangeNotifier {
@@ -25,6 +28,18 @@ public class MyNotifier extends AbstractStatusChangeNotifier {
 
         return Mono.fromRunnable(() -> {
             if (event instanceof InstanceStatusChangedEvent) {
+                StatusInfo statusInfo = ((InstanceStatusChangedEvent) event).getStatusInfo();
+                String status = statusInfo.getStatus();
+                Map<String, Object> details = statusInfo.getDetails();
+                String detailStr = details.toString();
+                boolean isOffline = statusInfo.isOffline();
+                LOGGER.info("status info are: status:{}, detail:{}, isOffline:{}", status, detailStr, isOffline);
+
+                String mavenVersion = instance.getBuildVersion().getValue();
+                String healthUrl = instance.getRegistration().getHealthUrl();
+                LOGGER.info("instance build version is {}, health check url is {}", mavenVersion, healthUrl);
+
+                // 获取事件信息，instance(客户端)信息，用来发钉钉消息，短信等等的通知
                 LOGGER.info("Instance {} ({}) is {}", instance.getRegistration().getName(), event.getInstance(),
                         ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus());
             } else {
