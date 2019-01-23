93a7e1b0ec943f778b185a0c6412c1f8bae092f7
GreedyPirate <18335166203@163.com>
2019-01-23 14:55:29 +0800

    java 8 Date time

---
+++ pom-test/src/test/java/com/ttyc/pom/test/json/User.java
@@ -0,0 +1,16 @@
+package com.ttyc.pom.test.json;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    private Date birthday;
+
+}
